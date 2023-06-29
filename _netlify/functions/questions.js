fb16b70d2397fa8d6575d157b49d2645e5d21fdd
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-06-29 21:25:33 +0300

    Update questions.js

--- _netlify/functions/questions.js
+++ _netlify/functions/questions.js
@@ -7,14 +7,12 @@ exports.handler =  async(event, context) => {
 
     return {
         statusCode:200,
-        headers:{
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Methods': '*',
-            'Access-Control-Max-Age': '2592000',
-            'Access-Control-Allow-Credentials': 'true',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTION",
+            "Content-Type": "application/json"
         },
+
 
         body:response,
     }
